464622f61b6fc947f14d09727b096de8c0747edc
keanucode <keanu.code@gmail.com>
2018-01-12 17:14:20 +0800

    ngx-bootstrap RWD 測試

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from "./app-routing.module";
 
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { AppComponent } from "./app.component";
 
-import { environment } from '../environments/environment';
+import { environment } from "../environments/environment";
+
+import { FormsModule } from "@angular/forms";
+
+import { ButtonsModule } from "ngx-bootstrap";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    ButtonsModule.forRoot(),
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register("/ngsw-worker.js", {
+      enabled: environment.production
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
